fb2cf4b37924fc191b960090b5f873131565de80
fedyna_krystyna <kristifedinasav@gmail.com>
2020-04-25 21:25:41 +0300

    init elemonts on econews page

--- src/test/java/com/softserve/edu/greencity/ui/pages/econews/EconewsPage.java
+++ src/test/java/com/softserve/edu/greencity/ui/pages/econews/EconewsPage.java
@@ -1,11 +1,32 @@
 package com.softserve.edu.greencity.ui.pages.econews;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.softserve.edu.greencity.ui.data.Languages;
 import com.softserve.edu.greencity.ui.pages.common.TopPart;
 
+/**
+ * 
+ * @author lv-493 Taqc/Java
+ *
+ */
 public class EconewsPage extends TopPart {
+	
+	//filters
+	private WebElement newsFilter;
+	private WebElement eventsFilter;
+	private WebElement courcesFilter;
+	private WebElement initiativesFilter;
+	private WebElement adsFilter;
+	
+	private WebElement createNewsButton;
+	
+	private WebElement gridViev;
+	private WebElement listViev;
+	
+	private ItemsContainer itemsContainer;
 
 	public EconewsPage(WebDriver driver) {
 		super(driver);
@@ -13,7 +34,15 @@ public class EconewsPage extends TopPart {
 	}
 
 	private void initElements() {
-		// init elements
+		
+		newsFilter = driver.findElement(By.xpath("//li[contains(text(), \"news\")]"));
+		eventsFilter = driver.findElement(By.xpath("//li[contains(text(), \"events\")]"));
+		courcesFilter = driver.findElement(By.xpath("//li[contains(text(), \"courses\")]"));
+		initiativesFilter = driver.findElement(By.xpath("//li[contains(text(), \"initiatives\")]"));
+		adsFilter = driver.findElement(By.xpath("//li[contains(text(), \"ads\")]"));
+		
+		createNewsButton = driver.findElement(By.id("create-button"));
+		itemsContainer = new ItemsContainer(driver);
 	}
 
 	// Page Object
