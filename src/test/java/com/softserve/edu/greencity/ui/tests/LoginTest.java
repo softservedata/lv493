a6ab95538d14cea8fd530a921e67e5db0625a37e
AndriyPytel <32392137+AndriyPytel@users.noreply.github.com>
2020-05-04 22:51:17 +0300

    fix typo mistake

--- src/test/java/com/softserve/edu/greencity/ui/tests/LoginTest.java
+++ src/test/java/com/softserve/edu/greencity/ui/tests/LoginTest.java
@@ -10,8 +10,8 @@ public class LoginTest extends GreencityTestRunner {
     @Test
     public void checkDropdown() { // for debugging
         LoginDropdown dropdown = loadApplication().gotoLoginDropdown();
-        dropdown.inputEmail("***")
-                .inputPassword("***")
+        dropdown.inputEmail("xoboj58975@2go-mail.com")
+                .inputPassword("Qwerty42?")
                 .gotoForgotPassword();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
     }
@@ -20,8 +20,8 @@ public class LoginTest extends GreencityTestRunner {
     public void checkPage() { // for debugging
         loadApplication().navigateMenuMyCabinet();
         LoginPage page = new LoginPage(driver);
-        page.inputEmail("***")
-                .inputPassword("***")
+        page.inputEmail("xoboj58975@2go-mail.com")
+                .inputPassword("Qwerty42?")
                 .clickLoginButton();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
     }
