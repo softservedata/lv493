b2ee0819c963317bca6dc082f1bfe8de475af2bb
Oksana <okstrishch@gmail.com>
2020-05-30 19:26:19 +0300

    changed invalid on faulty

--- src/test/java/com/softserve/edu/greencity/rest/data/IgnoreError400.java
+++ src/test/java/com/softserve/edu/greencity/rest/data/IgnoreError400.java
@@ -2,7 +2,7 @@ package com.softserve.edu.greencity.rest.data;
 
 public enum IgnoreError400 {
     EXIST_EMAIL("Subscriber with this email address exists in the database."), 
-    INVALID_EMAIL("непрвильно сформована email адреса"), 
+    FAULTY_EMAIL("непрвильно сформована email адреса"), 
     BAD_PASSWORD("Bad password"), 
     BAD_EMAIL("The user does not exist by this email"), 
     BLANK_FIELD("must not be blank"), 
