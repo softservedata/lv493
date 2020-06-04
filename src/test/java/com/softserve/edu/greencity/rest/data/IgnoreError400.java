a716d6407f89066ca9d704ddb7b20db4c140050b
Oksana <okstrishch@gmail.com>
2020-06-04 08:58:52 +0300

    added test delete

--- src/test/java/com/softserve/edu/greencity/rest/data/IgnoreError400.java
+++ src/test/java/com/softserve/edu/greencity/rest/data/IgnoreError400.java
@@ -3,7 +3,7 @@ package com.softserve.edu.greencity.rest.data;
 public enum IgnoreError400 {
     EXIST_EMAIL("Subscriber with this email address exists in the database."), 
     FAULTY_EMAIL("must be a well-formed email address"),
-    DELETED("Subscriber with this email address not found in the database."),
+    DELETED("Subscriber with this email address not found in the database.almyyhvxddxxnoct@ttirv.com"),
 //    FAULTY_EMAIL("непрвильно сформована email адреса"), 
     BAD_PASSWORD("Bad password"), 
     BAD_EMAIL("The user does not exist by this email"), 
