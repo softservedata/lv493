2795f31256cf310cf41378f52981d90bce003840
Oksana <okstrishch@gmail.com>
2020-06-05 16:45:06 +0300

    added set Methods for NewsSubscriberAllEntity

--- src/test/java/com/softserve/edu/greencity/rest/entity/NewsSubscriberAllEntity.java
+++ src/test/java/com/softserve/edu/greencity/rest/entity/NewsSubscriberAllEntity.java
@@ -1,32 +1,40 @@
 package com.softserve.edu.greencity.rest.entity;
 
-public class NewsSubscriberAllEntity  {
-     //admin
-    private String email;
-    private String unsubscribeToken;
- 
-    public NewsSubscriberAllEntity() {
-        email = "";
-        unsubscribeToken = "";
-    }
-    
-    public NewsSubscriberAllEntity(String email, String unsubscribeToken){
-        this.email = email;
-        this.unsubscribeToken = unsubscribeToken;
-    }
-    
-    public String getEmail() {
-        return email;
-    }
-    
-    public String getUnsubscribeToken() {
-        return unsubscribeToken;
-    }
-    
-    @Override
-    public String toString() {
-        return "NewsSubscriberAllEntity [email=" + email 
-                +"unsubscribeToken="+ unsubscribeToken +"]";
-    }
-    
-}
+public class NewsSubscriberAllEntity {
+    //admin
+      private String email;
+      private String unsubscribeToken;
+   
+      public NewsSubscriberAllEntity() {
+          email = "";
+          unsubscribeToken = "";
+      }
+      
+      public NewsSubscriberAllEntity(String email, String unsubscribeToken){
+          this.email = email;
+          this.unsubscribeToken = unsubscribeToken;
+      }
+      
+      public String getEmail() {
+          return email;
+      }
+      
+      public String getUnsubscribeToken() {
+          return unsubscribeToken;
+      }
+      
+      public void setEmail(String email) {
+          this.email = email;
+      }
+      
+      public void setUnsubscribeToken(String unsubscribeToken) {
+          this.unsubscribeToken = unsubscribeToken;
+      }
+      
+      @Override
+      public String toString() {
+          return "NewsSubscriberAllEntity [email=" + email 
+                  +"unsubscribeToken="+ unsubscribeToken +"]";
+      }
+      
+  }
