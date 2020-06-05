1e483d6c7738e6031fe0ac8c17be640b729648b8
Oksana <okstrishch@gmail.com>
2020-06-05 16:43:34 +0300

    Added methods for NewsSubscriberUnsubscribeEntity

--- src/test/java/com/softserve/edu/greencity/rest/entity/NewsSubscriberUnsubscribeEntity.java
+++ src/test/java/com/softserve/edu/greencity/rest/entity/NewsSubscriberUnsubscribeEntity.java
@@ -1,5 +1,32 @@
 package com.softserve.edu.greencity.rest.entity;
 
 public class NewsSubscriberUnsubscribeEntity {
+    private int number;
+    private String message;
+   
+    public NewsSubscriberUnsubscribeEntity() {
+        number = -1;
+        message = "";
+       
+    }
 
+    public NewsSubscriberUnsubscribeEntity(int number) {
+        this.number = number;
+      
+    }
+    public NewsSubscriberUnsubscribeEntity(String  message) {
+        this. message =  message;     
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    @Override
+    public String toString() {
+        return "NewsSubscriberUnsubscribeEntity [number= " + number + "message= " + message + "]";
+    }
 }
